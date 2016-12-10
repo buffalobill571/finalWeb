db6862cc04eb8d85b64b4d6a38b238e19873f0dd
buffalobill571 <bdabylovb@gmail.com>
2016-12-10 18:11:30 +0600

    Initial commit

---
+++ blog/static/blog/app/app.component.ts
@@ -0,0 +1,11 @@
+///<reference path="../node_modules/@types/node/index.d.ts"/>
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'my-app',
+  templateUrl: `app.component.html`,
+})
+export class AppComponent  {
+  name = 'Baha';
+}
